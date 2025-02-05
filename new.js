8ad0b958c9a777ad9633b1baf033485f0af7049b
DD <mohamedsabry71230432@gmail.com>
2025-02-05 09:03:03 +0200

    i added all files already

---
+++ new.js
@@ -0,0 +1,10 @@
+let iphone=document.querySelector(".iphone");
+let container=document.querySelector(".container")
+
+function phone(x){
+    iphone.src= x;
+    
+}
+function colors(color){
+    container.style.background =color;
+}
